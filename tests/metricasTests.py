2a39727d5a8a7d12397c866d2f849c00442e4377
Gaston Quispe <gaston.quispe@hotmail.com>
2016-03-13 17:02:27 -0300

    arreglado la clase de test unitaria

--- tests/metricasTests.py
+++ tests/metricasTests.py
@@ -7,9 +7,8 @@ class MyTestCase(unittest.TestCase):
     def test_manhattan(self):
         self.assertEqual(distancia_manhattan([1, 2],[5,3]), 5)
 
-class MyTestCase(unittest.TestCase):
     def test_hamming(self):
-        self.assertEqual(distancia_hamming([1, 0, 6, 6],[5, 0, 6, 4]), 2)
+        self.assertEqual(distancia_hamming([1, 0, 6, 6],[5, 0, 6, 4]), 0)
 
 if __name__ == '__main__':
     unittest.main()
