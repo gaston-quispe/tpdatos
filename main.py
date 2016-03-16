519386751fef6aeb9544e32f18739297dd9d670d
valeriamrb <valeria.mrb@gmail.com>
2016-03-16 16:43:52 -0300

    Se genera el archivo de prototipos para usar como train

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from src.knn import knn
 from src.metricas import distancia_manhattan
 from src.utiles import imprimir_imagenes_test
 from src.forward_editing import forward_editing
+import csv
 
-imprimir_imagenes_test("data/_test10elem.csv")
-start = time.time()
-listaclases = knn("data/_train100elem.csv", "data/_test10elem.csv", distancia_manhattan, 3)
-end = time.time()
+#imprimir_imagenes_test("data/_test10elem.csv")
+#start = time.time()
+#listaclases = knn("data/_train100elem.csv", "data/_test10elem.csv", distancia_manhattan, 3)
+#end = time.time()
 
-print end - start
-print listaclases
+#print end - start
+#print listaclases
 
 print "Prueba editing"
 print "Lista de prototipos: "
@@ -21,3 +22,16 @@ for i in prototipos:
     cantidad_prototipos += 1
 
 print "Cantidad de prototipos: " + str(cantidad_prototipos)
+
+archivo = open("_archivoPrototipos.csv", "w")
+archivo_csv = csv.writer(archivo)
+archivo_csv.writerows(prototipos)
+archivo.close()
+
+imprimir_imagenes_test("data/_test10elem.csv")
+start = time.time()
+listaclases = knn("_archivoPrototipos.csv", "data/_test10elem.csv", distancia_manhattan, 3)
+end = time.time()
+
+print end - start
+print listaclases
