dba366a773f96829dc9fe33f8b9409409ad3d9bf
valeriamrb <valeria.mrb@gmail.com>
2016-03-16 18:28:29 -0300

    Arreglo de Grid Search

--- src/grid_search.py
+++ src/grid_search.py
@@ -1,16 +1,18 @@
 import metricas
-import knn
+from knn import knn
 
 
-def grid_search(elementToclassify, cantidad_observaciones):
+def grid_search(traincsv, clases, cantidad_observaciones):
     mejor_precision = 0; mejor_k = 0; mejor_metrica = 0
     lista_metricas = [metricas.distancia_euclideana(), metricas.distancia_manhattan(), metricas.distancia_hamming()]
     for k in range(0,100):
         for metrica in lista_metricas:
-            resultadoKnn = knn.knn_1element(metrica, k, elementToclassify)
-            precision = sum(resultadoKnn)/cantidad_observaciones
+            prediccionKnn = knn(traincsv, clases, metrica, k, )
+            precision = sum(prediccionKnn == clases)/cantidad_observaciones
             if(precision > mejor_precision):
                 mejor_precision = precision
                 mejor_k = k
                 mejor_metrica = metrica
+
+    return mejor_k, mejor_metrica
 
